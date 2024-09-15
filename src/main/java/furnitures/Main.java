6a013510820e65cff1fcc8e7667a3bde92901632
Boyan Mihovski <bmihovski@live.com>
2024-09-15 08:55:56 +0300

    add example test code java

--- src/main/java/furnitures/Main.java
+++ src/main/java/furnitures/Main.java
@@ -1,5 +1,9 @@
 package furnitures;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
 /** This is the main class of the application. It is the entry point of the program. */
 public class Main {
   /**
@@ -8,8 +12,18 @@ public class Main {
    * @param args the input arguments
    */
   public static void main(String[] args) {
-    System.out.println("test");
-    // Engine engine = new EngineImpl();
-    // engine.run();
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    scanner.close();
+    Deque<Integer> stack = new ArrayDeque<>();
+    for (int start = 0; start < input.length(); ++start) {
+      char ch = input.charAt(start);
+      if (ch == '(') {
+        stack.push(start);
+      } else if (ch == ')') {
+        var match = input.substring(stack.pop(), start + 1);
+        System.out.println(match);
+      }
+    }
   }
 }
